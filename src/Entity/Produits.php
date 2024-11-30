7264547577be7402ed68dcce8bc1b314fff9ba22
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-11-30 21:31:34 +0100

    Initialisation de l'app

---
+++ src/Entity/Produits.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitsRepository::class)]
+class Produits
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prix = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $catégorie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $date_de_création = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getCatégorie(): ?string
+    {
+        return $this->catégorie;
+    }
+
+    public function setCatégorie(string $catégorie): static
+    {
+        $this->catégorie = $catégorie;
+
+        return $this;
+    }
+
+    public function getDateDeCréation(): ?string
+    {
+        return $this->date_de_création;
+    }
+
+    public function setDateDeCréation(string $date_de_création): static
+    {
+        $this->date_de_création = $date_de_création;
+
+        return $this;
+    }
+}
