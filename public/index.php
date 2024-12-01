bbed22f8c343310946b92a1307b1ea5696cf3070
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-01 22:48:15 +0100

    Connexion réussie teachful_db

--- public/index.php
+++ public/index.php
@@ -2,14 +2,29 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Initialisation de Faker
 use Faker\Factory;
 
-$faker = Factory::create('fr_FR');
+// Connexion à la base de données
+$host = '127.0.0.1';
+$db   = 'teachful_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    // Connexion à la base de données avec PDO
+    $pdo = new PDO($dsn, $user, $pass);
+    echo "Connexion réussie à la base de données !";
+} catch (\PDOException $e) {
+    // En cas d'erreur, afficher l'erreur
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+
+
+
+?>
