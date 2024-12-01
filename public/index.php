0e95e9b01db1193ab925784df3d7dd63f260f9f2
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-01 21:20:33 +0100

    installation de Faker

--- public/index.php
+++ public/index.php
@@ -3,6 +3,12 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Initialisation de Faker
+use Faker\Factory;
+
+$faker = Factory::create('fr_FR');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
