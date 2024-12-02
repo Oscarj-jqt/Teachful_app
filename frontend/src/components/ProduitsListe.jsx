5e8e4b8fa85c3e6e22298b662a185f13a9a067ba
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-02 19:21:15 +0100

    Fonctionnalité ajout produits

--- frontend/src/components/ProduitsListe.jsx
+++ frontend/src/components/ProduitsListe.jsx
@@ -27,9 +27,11 @@ function ProduitsListe() {
         event.preventDefault();
 
         const nouveauProduit = {
+            // les attributs de l'entité
             nom,
             description,
             prix, 
+            categorie_relation_id: categorieId,
         };
 
     // Envoie de la requête API pour ajouter
@@ -47,7 +49,7 @@ function ProduitsListe() {
         setProduits((prevProduits) => [...prevProduits, data]);
         setNom('');
         setDescription('');
-        setPrix('');
+        setPrix();
         setCategorieId('');
       })
       .catch((err) => console.error(err));
@@ -57,16 +59,42 @@ function ProduitsListe() {
         return (
             <div>
               <h1>Liste des Produits</h1>
+              <form onSubmit={handleAddProduct}>
+                <div>
+                    <label>Nom</label>
+                    <input type="text" value={nom} onChange={(e) => setNom(e.target.value)} required/>
+                </div>
+                <div>
+                    <label>Description</label>
+                    <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required/>
+                </div>
+                <div>
+                    <label>Prix</label>
+                    <input type="number" value={prix} onChange={(e) => setPrix(e.target.value)} required />
+                </div>
+                <div>
+                    <label>Catégorie</label>
+                    <select value={categorieId} onChange={(e) => setCategorieId(e.target.value)} required>
+                    <option value="">Sélectionner une catégorie</option>
+                    {categorieId.map((categorie) => (
+                        <option key={categorie.id} value={categorie.id}>
+                            {categorie.nom}
+                        </option>
+                    ))}
+                    </select>
+                </div>
+                    <button type="submit">Ajouter le produit</button>
+                </form>
 
 
-
-              
+                <h2>Les Produits</h2>
                 <table>
                   <thead>
                     <tr>
                       <th>Nom</th>
                       <th>Description</th>
                       <th>Prix</th>
+                      <th>Catégorie</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -75,6 +103,7 @@ function ProduitsListe() {
                         <td>{produit.nom}</td>
                         <td>{produit.description}</td>
                         <td>{produit.prix}</td>
+                        <td>{produit.categorie.nom}</td>
                       </tr>
                     ))}
                   </tbody>
