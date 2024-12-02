aaace0b61141f449f10e531439177893aeee6310
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-02 19:03:30 +0100

    Fonctionnalité ajout produits

--- frontend/src/components/ProduitsListe.jsx
+++ frontend/src/components/ProduitsListe.jsx
@@ -1,19 +1,66 @@
 import { React, useEffect, useState } from "react";
-import { fetchProduits } from "../api";
+import { api_url } from '../api.js';
 
+// Composant qui gère l'affichage et les opération CRUD des produits
 function ProduitsListe() {
+    // initialisation de l'état (tableau produit)
     const [produits, setProduits] = useState([]);
 
-        // Au chargement de la page on va charger les produits
-        useEffect(() => {
-            fetchProduits()
-            .then((data) => setProduits(data));
-        }, []);
+    // initialisation des attributs
+    const [nom, setNom] = useState('');
+    const [description, setDescription] = useState('');
+    // float
+    const [prix, setPrix] = useState();
+    const [categorieId, setCategorieId] = useState('');
+
+    // Au chargement de la page on va charger les produits (GET)
+    useEffect(() => {
+        // Fetch produits
+        fetch(`${api_url}/api/produits`)
+          .then((res) => res.json())
+          .then((data) => setProduits(data))
+    }, []);
+
+    // Fonction d'ajout des produits
+    const handleAddProduct = (event) => {
+        // fonctionnalité js 
+        event.preventDefault();
+
+        const nouveauProduit = {
+            nom,
+            description,
+            prix, 
+        };
+
+    // Envoie de la requête API pour ajouter
+    fetch(`${api_url}/api/produits`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(nouveauProduit),
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        // appel du setteur pour ajouter le nouveau produit
+        // méthode .. React
+        setProduits((prevProduits) => [...prevProduits, data]);
+        setNom('');
+        setDescription('');
+        setPrix('');
+        setCategorieId('');
+      })
+      .catch((err) => console.error(err));
+    };
 
 
         return (
             <div>
               <h1>Liste des Produits</h1>
+
+
+
+              
                 <table>
                   <thead>
                     <tr>
