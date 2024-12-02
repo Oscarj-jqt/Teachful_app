f500d5cd99d203c8ed8be31e7c21f521019e3a7a
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-02 19:05:37 +0100

    Fonctionnalité ajout produits

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,20 +3,20 @@
 // On récupère tout les produits
 const api_url = 'http://127.0.0.1:8000 ';
 
-export const fetchProduits = async () => {
-  const response = await fetch(`${api_url}/api/produits`);
-  if (!response.ok) {
-    throw new Error('Erreur lors de la récupération des produits');
-  }
-  return await response.json();
-    };
+// export const fetchProduits = async () => {
+//   const response = await fetch(`${api_url}/api/produits`);
+//   if (!response.ok) {
+//     throw new Error('Erreur lors de la récupération des produits');
+//   }
+//   return await response.json();
+//     };
 
 
-// Maintenant les catégories
-export const fetchCategories = async () => {
-  const response = await fetch(`${api_url}/api/categories`);
-  if (!response.ok) {
-    throw new Error('Erreur lors de la récupération des catégories');
-  }
-  return await response.json();
-  };
+// // Maintenant les catégories
+// export const fetchCategories = async () => {
+//   const response = await fetch(`${api_url}/api/categories`);
+//   if (!response.ok) {
+//     throw new Error('Erreur lors de la récupération des catégories');
+//   }
+//   return await response.json();
+//   };
