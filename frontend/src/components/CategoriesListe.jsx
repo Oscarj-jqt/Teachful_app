298669b3b58c2d17b173780d5bd2afe20cfed3fb
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-02 22:23:47 +0100

    Fonctionnalité ajout catégories

--- frontend/src/components/CategoriesListe.jsx
+++ frontend/src/components/CategoriesListe.jsx
@@ -1,17 +1,60 @@
 import { React, useEffect, useState } from "react";
-import { fetchCategories } from "../api";
+
 
 function CategoriesListe() {
-  const [categories, setCategories] = useState([]);
+    // initialisation de l'état d'affichage (tableau categories)
+    const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    fetchCategories()
+    // état pour les attributs
+    const [nom, setNom] = useState("");
+
+    useEffect(() => {
+    // récupération des données des catégories
+    fetch('http://127.0.0.1:8000/api/categories')
+      .then((res) => res.json())
       .then((data) => setCategories(data))
-  }, []);
+    }, []);
+
+    // Fonction d'ajout d'une nouvelle catégorie
+    const ajoutCategorie = (e) => {
+        e.preventDefault();
+    
+        const nouvelleCategorie = {
+          nom: nom,
+        };
+
+    // Envoie de la requête API pour ajouter la catégorie
+    fetch("http://127.0.0.1:8000/api/categories", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(nouvelleCategorie),
+
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            // Màj de la liste des catégories
+            setCategories((prevCategories) => [...prevCategories, data]);
+            setNom("");
+        })
+        .catch((err) => console.error(err));
+
+};
 
   return (
     <div>
       <h1>Liste des Catégories</h1>
+
+      {/* Formulaire pour ajouter une catégorie */}
+      <form onSubmit={ajoutCategorie}>
+        <div>
+          <label>Nom de la catégorie</label>
+          <input type="text" value={nom} onChange={(e) => setNom(e.target.value)} required/>
+        </div>
+        <button type="submit">Ajouter la catégorie</button>
+      </form>
+
         <ul>
           {categories.map((categorie) => (
             <li key={categorie.id}>{categorie.nom}</li>
