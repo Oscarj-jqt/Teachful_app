6b1b858b3c5ef6d256f7286a25aec7af2084c11f
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-03 12:21:15 +0100

    Intégration charte graphique dans la configuration Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Bleu foncé 
+        primary: '#004066',
+        // Bleu clair 
+        secondary: '#219CFF',
+        // Orange foncé 
+        accent: '#CD694F',
+        // Orange clair 
+        highlight: '#EC704D',
+        white: '#FFFFFF',
+        black: '#000000',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
-}
+};
 
