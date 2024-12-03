4dffa01f2701af48c265b78e45000fc36aa86d68
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-03 21:05:23 +0100

    Création d'une route de connexion pour obtenir un token JWT

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController
+{
+    // Première route correspondant à la connexion pour avoir un token JWT
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function login(): Response
+    {
+        // Cette route sera interceptée par le firewall JWT
+        return new Response('', Response::HTTP_UNAUTHORIZED);
+    }
+}
