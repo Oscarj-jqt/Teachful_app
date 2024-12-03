c7067943b9296a82682c8892181a57ed1454b5a5
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-03 18:06:27 +0100

    Ajout système de gestion d'erreur avec Redux pour l'epérience de l'utilisateur

--- frontend/src/redux/reducers/produitsReducer.js
+++ frontend/src/redux/reducers/produitsReducer.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 // État initial de notre slice
 const initialState = {
   produits: [],
+  erreur: null,
 };
 
 // Création du slice
@@ -14,23 +15,32 @@ const produitsSlice = createSlice({
     // Action pour ajouter un produit
     ajouterProduit: (state, action) => {
       state.produits.push(action.payload);
+      state.erreur = null;
     },
     // Action pour supprimer un produit
     supprimerProduit: (state, action) => {
       state.produits = state.produits.filter(produit => produit.id !== action.payload);
+      state.erreur = null;
     },
     // Action pour modifier un produit
     modifierProduit: (state, action) => {
       const index = state.produits.findIndex(produit => produit.id === action.payload.id);
       if (index !== -1) {
         state.produits[index] = action.payload;
+        state.erreur = null;
       }
     },
+    setErreurProduit: (state, action) => {
+      state.erreur = action.payload; // Action pour définir l'erreur
+    },
+    resetErreurProduit: (state) => {
+      state.erreur = null; // Action pour réinitialiser l'erreur
+    }
   },
 });
 
 // Export des actions générées par createSlice
-export const { ajouterProduit, supprimerProduit, modifierProduit } = produitsSlice.actions;
+export const { ajouterProduit, supprimerProduit, modifierProduit, setErreurProduit, resetErreurProduit } = produitsSlice.actions;
 
 // Export du reducer
 export default produitsSlice.reducer;
